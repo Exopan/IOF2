89d413f10f0a755b56df8c71f6f56685b14e1201
Exopan <exopanDE@gmail.com>
2016-07-22 09:52:00 +0200

    Add files via upload

---
+++ __init__.lua
@@ -0,0 +1,8 @@
+-- note to self: least dependant first
+import "Turbine.Gameplay";
+import "Turbine.UI.Lotro";
+import "Almagnus1.IOF.Class";
+import "Almagnus1.IOF.EmoteManager";
+import "Almagnus1.IOF.SettingManager";
+import "Almagnus1.IOF.IOFWindow";
+import "Almagnus1.IOF.CryptWindow";
